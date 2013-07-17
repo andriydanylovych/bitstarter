e6e3c684aced378702f9d7304280d80b1af73151
Andriy Danylovych <andriy.danylovych@gmail.com>
2013-07-17 23:45:57 +0200

    Update web.js

--- web.js
+++ web.js
@@ -6,6 +6,8 @@ app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
 
+fs.readFileSync('/index.html');
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
